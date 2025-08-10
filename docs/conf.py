7be6fccd44f3f78434402e2168c46f18ea86c88b
Ish Pen <ishpen19@gmail.com>
2025-08-09 19:08:41 -0500

    readthedocs

---
+++ docs/conf.py
@@ -0,0 +1,12 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'handposeutils'
+author = 'Ishan'
+release = '0.1.1'
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+templates_path = ['_templates']
+exclude_patterns = []
+html_theme = 'sphinx_rtd_theme'
